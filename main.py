15cc8007346210650a081c4b289b1db4ad4d8031
fraukecharms <frharms@gmail.com>
2022-09-25 14:08:12 +0000

    successfully combined rekognition and fastapi <3{ }

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
 import uvicorn
+from lambdatest import label_function
+import boto3
+import io
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Duke"}
+    return {"message": "Hello there ... append '/docs' to url"}
 
-@app.get("/add/{num1}/{num2}")
-async def add(num1: int, num2: int):
-    """Add two numbers together"""
+@app.post("/predict1")
+async def lookup1(name: str):
+    """label image in s3 bucket"""
+    bucket = 'compvision-lambdatrigger-bucket'
+    
 
-    total = num1 + num2
-    return {"total": total}
+    return label_function(bucket, name)
+    
+    
+@app.post("/predict2")
+async def lookup2(photo: UploadFile = File(...)):
+    """upload image"""
+    
+    client = boto3.client("rekognition")
+
+    response = client.detect_labels(Image={'Bytes': photo.file.read()})
+        
+    return response
+    
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
